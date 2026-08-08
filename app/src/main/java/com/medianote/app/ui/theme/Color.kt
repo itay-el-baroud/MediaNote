4ca884eec8dccf987993a85cc2afed9ea7bcb85d
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-09 01:18:42 +0300

    Add new color constants for theme

--- app/src/main/java/com/medianote/app/ui/theme/Color.kt
+++ app/src/main/java/com/medianote/app/ui/theme/Color.kt
@@ -1,13 +1,11 @@
 package com.medianote.app.ui.theme
-
 import androidx.compose.ui.graphics.Color
-
 val PrimaryPurple = Color(0xFF7C4DFF)
+val PrimaryDark = Color(0xFF5A2AC0)
 val SecondaryPurple = Color(0xFFB388FF)
+val ControlHighlight = Color(0xFF7C4DFF)
+val ControlNormal = Color(0xFFB0B0B0)
 val BackgroundDark = Color(0xFF121212)
 val SurfaceDark = Color(0xFF1E1E1E)
 val BackgroundLight = Color(0xFFF5F5F5)
 val SurfaceLight = Color(0xFFFFFFFF)
-val TextPrimaryDark = Color(0xFFFFFFFF)
-val TextSecondaryDark = Color(0xFFB0B0B0)
-val ErrorRed = Color(0xFFCF6679)
