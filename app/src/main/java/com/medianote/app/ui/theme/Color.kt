b7bd688581d065201985a7ac3e1b298ac0b497cd
itay-el-baroud <omarmxndncbbf@gmail.com>
2026-08-08 23:52:16 +0300

    Add color definitions for app theme

---
+++ app/src/main/java/com/medianote/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.medianote.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryPurple = Color(0xFF7C4DFF)
+val SecondaryPurple = Color(0xFFB388FF)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val BackgroundLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color(0xFFFFFFFF)
+val TextPrimaryDark = Color(0xFFFFFFFF)
+val TextSecondaryDark = Color(0xFFB0B0B0)
+val ErrorRed = Color(0xFFCF6679)
